873ac0f3838e6cb984e1275f03ea0ce12ce50dcc
Vineet3-2-1 <vineetpun321@gmail.com>
2025-08-18 21:41:01 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,7 @@
+# Python program to perform operations using Division and Modulo operators
+
+a , b = 10 , 3
+
+print(f"The division of {a} and {b} = {a/b}")
+print(f"The floor division of {a} and {b} = {a//b}")
+print(f"The modulo/remainder of {a} and {b} when performed = {a%b}")
---
+++ 6.py
@@ -0,0 +1,4 @@
+# Program to show the work of escape sequece like \n and \t
+
+print("Jesus Christ is GOD our LORD.\nAmen")
+print("My name is \tVineet")
